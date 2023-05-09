fb68558f54187c2b65caf23fd7ade2bcea94f15c
Ariva2 <ariva080503@gmail.com>
2023-05-09 14:00:57 +0700

    Perulangan Do While

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ public class Main {
         System.out.println("Isi Gelas Sekarang : " + isiGelas + "m1");
         System.out.println("Isi Gelas Ketika Penuh : " + penuh + "m1");
 
-        while(isiGelas != penuh){
+        do {
             isiGelas++;
             System.out.println("Sedang mengisi gelas..");
             System.out.println("Isi Gelap Sekrang : " + isiGelas + "m1");
-        }
+        }while(isiGelas != penuh);
 
         System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "m1");
 
